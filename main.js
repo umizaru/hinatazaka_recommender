8e9f269b720500f4357d8aadd1013fc8b788da7d
umizaru <y.wakita1T@gmail.com>
2023-05-28 17:58:01 +0900

    推し1人のときの実装完了

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-// import data from "./memberdata.json";
+import { generateKey } from "crypto";
+import membersData from "./memberdata.json" assert { type: "json" };
 import pkg from "enquirer";
 const { prompt } = pkg;
 import fs from "fs";
@@ -11,7 +12,7 @@ async function hinatazakaRecommender() {
     type: "select",
     name: "preparation",
     message: "準備はいいですか？",
-    choices: ["はい", "いいえ"],
+    choices: ["はい"],
   });
 
   const answer1 = await prompt({
@@ -23,14 +24,14 @@ async function hinatazakaRecommender() {
 
   const answer2 = await prompt({
     type: "select",
-    name: "from",
+    name: "from_region",
     message: "どの出身地がいいですか？",
     choices: ["関東", "関西", "地方"],
   });
 
   const answer3 = await prompt({
     type: "select",
-    name: "height",
+    name: "height_group",
     message: "身長はどうですか？",
     choices: ["高", "中", "低"],
   });
@@ -46,7 +47,70 @@ async function hinatazakaRecommender() {
     type: "select",
     name: "looks",
     message: "見た目は？",
-    choices: ["かわいい", "きれい"],
+    choices: ["きれい", "かわいい"],
   });
+
+  let memberPoints = {};
+
+  membersData.forEach(function (member) {
+    memberPoints[member.name] = 0;
+  });
+
+  const { generation } = answer1;
+  const { from_region } = answer2;
+  const { height_group } = answer3;
+  const { character } = answer4;
+  const { looks } = answer5;
+
+  for (const member of membersData) {
+    if (member.generation === generation) {
+      memberPoints[member.name] += 1;
+    }
+  }
+  for (const member of membersData) {
+    if (member.from_region === from_region) {
+      memberPoints[member.name] += 1;
+    }
+  }
+  for (const member of membersData) {
+    if (member.height_group === height_group) {
+      memberPoints[member.name] += 1;
+    }
+  }
+  for (const member of membersData) {
+    if (member.character === character) {
+      memberPoints[member.name] += 1;
+    }
+  }
+  for (const member of membersData) {
+    if (member.looks === looks) {
+      memberPoints[member.name] += 1;
+    }
+  }
+
+  let maxPoint = 0;
+  let recommendedMembers = [];
+
+  for (const member in memberPoints) {
+    if (memberPoints[member] > maxPoint) {
+      maxPoint = memberPoints[member];
+      recommendedMembers = member;
+    } else if (memberPoints[member] === maxPoint) {
+      recommendedMembers.push(member);
+    }
+  }
+
+  // if (recommendedMembers.length > 2) {
+  //   let selectedMember =
+  //     recommendedMembers[Math.floor(Math.random() * recommendedMembers.length)];
+  //   console.log(selectedMember);
+  // }
+
+  let selectedMember = membersData.find(
+    (element) => element.name === recommendedMembers
+  );
+  console.log("メンバー名:", selectedMember.name);
+  console.log("キャッチフレーズ:", selectedMember.catchphrase);
+  console.log("ブログURL:", selectedMember.url);
 }
 hinatazakaRecommender();
