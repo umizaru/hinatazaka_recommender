2c44421b9498be9baeae3ac5ddfba9db552788a4
umizaru <y.wakita1T@gmail.com>
2023-06-14 17:19:02 +0900

    全体の命名等を修正、選択肢の文言を修正

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const questionData = JSON.parse(await readFile("./questiondata.json"));
 async function hinatazakaRecommender() {
   console.log("日向坂レコメンダーへようこそ!!");
   console.log(
-    "5つの質問に答えて、自分にぴったりの日向坂メンバーを見つけよう!!"
+    "5つの質問に答えて、自分にぴったりの日向坂メンバーを見つけよう!!\n"
   );
 
   const preparation = await prompt({
@@ -15,12 +15,6 @@ async function hinatazakaRecommender() {
     name: "preparation",
     message: "準備ができたら[はい]を推してください",
     choices: ["はい"],
-  });
-
-  let memberPoints = {};
-
-  memberData.forEach(function (member) {
-    memberPoints[member.name] = 0;
   });
 
   const answers = {};
@@ -34,18 +28,19 @@ async function hinatazakaRecommender() {
     answers[name] = answer;
   }
 
+  let memberPoints = {};
+  memberData.forEach((member) => (memberPoints[member.name] = 0));
   for (const member of memberData) {
-    const { generation, from_region, height_group, character, looks } = answers;
+    const { generation, birthplace, height, character, looks } = answers;
     if (member.generation === generation) memberPoints[member.name] += 1;
-    if (member.from_region === from_region) memberPoints[member.name] += 1;
-    if (member.height_group === height_group) memberPoints[member.name] += 1;
+    if (member.birthplace === birthplace) memberPoints[member.name] += 1;
+    if (member.height === height) memberPoints[member.name] += 1;
     if (member.character === character) memberPoints[member.name] += 1;
     if (member.looks === looks) memberPoints[member.name] += 1;
   }
 
   let maxPoint = 0;
   let recommendedMembers = [];
-
   for (const member in memberPoints) {
     if (memberPoints[member] > maxPoint) {
       recommendedMembers = [];
@@ -59,7 +54,7 @@ async function hinatazakaRecommender() {
   const recommendedMember =
     recommendedMembers[Math.floor(Math.random() * recommendedMembers.length)];
   const selectedMember = memberData.find(
-    (element) => element.name === recommendedMember
+    (member) => member.name === recommendedMember
   );
 
   function delay(ms) {
@@ -69,14 +64,13 @@ async function hinatazakaRecommender() {
   }
 
   async function result() {
-    console.log("あなたにぴったりの日向坂メンバーは…\n");
+    console.log("あなたにオススメの日向坂メンバーは…\n");
     await delay(1000);
     console.log(`${selectedMember.catchphrase}${selectedMember.name}だ!!\n`);
-    await delay(2000);
-    console.log("メンバーのブログもチェックしてみてね!!");
+    await delay(1000);
+    console.log("メンバーのブログはこちら↓");
     console.log(selectedMember.url);
   }
-
   result();
 }
 hinatazakaRecommender();
--- test.js
+++
@@ -1,3 +0,0 @@
-const array = [1, 2, 3, 4, 5];
-const randomElement = array[Math.floor(Math.random() * array.length)];
-console.log(randomElement);
